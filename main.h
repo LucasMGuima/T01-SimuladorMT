1b73dc1c47ee5d4cf81a2c676d61bc13c6db8ffd
Lucas <lucasdamata01@hotmail.com>
2024-09-28 12:57:56 -0300

    Limpeza

--- main.h
+++ main.h
@@ -4,15 +4,6 @@
 #define TRUE 1
 #define FALSE 0
 
-//Declaracao de variavel
-unsigned char alfabeto[30];
-unsigned short int qtdEstadoes;
-unsigned short int qtdTransicoes;
-unsigned short int nPalavras;
-unsigned char fita[100];
-unsigned char palavra[100];
-unsigned char temp_fita[100];
-
 //Declaracao de funcoes
 int main();
 
